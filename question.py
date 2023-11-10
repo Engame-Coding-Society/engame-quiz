f8f0409b04228df6b9b19223193f1398a6892c7e
Rákos Ádám Dominik <rakos.adam.d@gmail.com>
2023-11-10 17:03:26 +0100

    Created question loading

--- question.py
+++ question.py
@@ -1,3 +1,6 @@
+import yaml
+
+PROMPTS = []
 
 
 class Question:
@@ -14,3 +17,22 @@ class Question:
         """
         self.question = question
         self.options = options
+
+
+def load_questions():
+    """
+    Loads every question from the 'questions.yml' file.
+    """
+    PROMPTS.clear()
+    with open("questions.yml", 'r') as file:
+        prompts = yaml.safe_load(file)
+        for prompt in prompts["questions"]:
+            question = prompt["question"]
+            answers = prompt["answers"]
+            if not isinstance(question, str):
+                print(f"Got incorrect type for question: {type(question)}")
+                continue
+            if not isinstance(answers, list):
+                print(f"Got incorrect type for answer: {type(answers)}")
+                continue
+            PROMPTS.append(Question(question, answers))

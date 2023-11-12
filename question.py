e2f92dd5b277f7c73a25cb74a934b4858d30e306
ProgrammerPeter2 <hp.raszpi@gmail.com>
2023-11-12 11:41:11 +0100

    #10 separated the test part of the task

--- question.py
+++ question.py
@@ -4,12 +4,12 @@ class Question:
     See params in the `__init__` method.
     """
 
-    def __init__(self, question, options, correct_idx):
+    def __init__(self, question, options, correct_idx=0):
         """
         The constructor of the question model
         :param question: The title (the question itself)
         :param options: The list of possible answers
-        :param correct_idx: The index of the correct answer
+        :param correct_idx: The index of the correct answer. 0 by default.
         """
         self.question = question
         self.options = options
@@ -21,28 +21,3 @@ class Question:
         :param answer_idx: The index of the answer chosen by the player.
         """
         return answer_idx == self.correctIdx
-
-    @classmethod
-    def load(cls) -> 'Question':
-        """
-        Create a sample question.
-        :return: A :class:`Question` instance
-        """
-        question = "What programming language are you using now?"
-        options = ["Python", "Javascript", "HTML", "C#"]
-        correct_idx = 0
-        return cls(question, options, correct_idx)
-
-    def play(self):
-        print(self.question)
-        print("Options:", self.options)
-
-        # Get player's answer
-        answer_idx = int(input("Enter the index of your answer: "))
-
-        # Check if the answer is correct
-        print(self.is_correct(answer_idx))
-
-
-loaded_question = Question.load()
-loaded_question.play()

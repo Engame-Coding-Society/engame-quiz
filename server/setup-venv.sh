c41e4b9f01575212d031843c608159c18d9b3a11
ProgrammerPeter2 <hp.raszpi@gmail.com>
2024-01-05 12:39:08 +0100

    an attempt to set up the python venv properly

--- server/setup-venv.sh
+++ server/setup-venv.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-python3.11 -m venv venv
-source venv/bin/activate
-pip3 install --upgrade pip
+if [ $# -ge 0 ]
+then
+  if [ "$1" = "local" ]
+  then
+    python3.11 -m venv venv
+    source venv/bin/activate
+    pip3 install --upgrade pip
+  elif [ "$1" = "actions" ]
+  then
+    source /github/workspace/server/venv/bin/activate
+  fi
+fi
+
 python3.11 -m pip install -r requirements.txt
